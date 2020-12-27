60223ccbf64563010cb70f55f321dfa2f0ad54ef
BarBot <74660569+BarBot-spec@users.noreply.github.com>
2020-12-27 23:44:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -219,6 +219,8 @@ async function starts() {
 					break
 				case 'stiker':
 				case 'sticker':
+                                case 'sgif':
+                                case 's':
 					if ((isMedia && !mek.message.videoMessage || isQuotedImage) && args.length == 0) {
 						const encmedia = isQuotedImage ? JSON.parse(JSON.stringify(mek).replace('quotedM','m')).message.extendedTextMessage.contextInfo : mek
 						const media = await client.downloadAndSaveMediaMessage(encmedia)

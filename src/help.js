878c40f220eb8336c6ddf4eb84981072c67044c3
BarBot <74660569+BarBot-spec@users.noreply.github.com>
2021-01-01 01:36:52 +0800

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,193 @@
+const help = (prefix) => { 
+	return ` _*>BarXnl_BOT<*_                
+
+
+ Hi, _*undefined*_ 
+ Berikut adalah fitur pada bot!
+_*Fun*_
+├ *${prefix}tebakgambar*
+├ *${prefix}caklontong*
+├ *${prefix}family100*
+├ *${prefix}game*
+├ *${prefix}truth*
+├ *${prefix}dare*
+├ *${prefix}quotes*
+├ *${prefix}hilih*
+├ *${prefix}alay* [text]
+├ *${prefix}simi* [text]
+├ *${prefix}bucin*
+├ *${prefix}gtts* [text]
+└ *${prefix}tts*
+
+_*Kerang*_
+├ *${prefix}apakah*
+├ *${prefix}bisakah*
+├ *${prefix}kapankah*
+└ *${prefix}rate*
+
+_*Wibu*_
+├ *${prefix}randomanime*
+├ *${prefix}waifu*
+├ *${prefix}waifu2*
+├ *${prefix}nakonime*
+├ *${prefix}wibu*
+├ *${prefix}wait*
+├ *${prefix}inu*
+├ *${prefix}pokemon*
+├ *${prefix}naruto*
+├ *${prefix}hinata*
+├ *${prefix}sasuke*
+├ *${prefix}sakura*
+├ *${prefix}boruto*
+├ *${prefix}minato*
+├ *${prefix}loli*
+├ *${prefix}loli2*
+├ *${prefix}rize*
+├ *${prefix}akira*
+├ *${prefix}itori*
+├ *${prefix}kurumi*
+└ *${prefix}miku*
+
+_*Nsfw Menu*_
+├ *${prefix}hentai*
+├ *${prefix}randomhentai*
+├ *${prefix}nsfwblowjob*
+├ *${prefix}nsfwtrap*
+├ *${prefix}nsfwneko*
+
+_*Sticker*_
+├ *${prefix}sticker*
+├ *${prefix}stiker*
+├ *${prefix}stickergif*
+├ *${prefix}sgif*
+├ *${prefix}s*
+├ *${prefix}toimg*
+├ *${prefix}tstiker*
+└ *${prefix}tsticker*
+
+_*Search*_
+├ *${prefix}ytsearch* [yt search]
+├ *${prefix}pinterest* [text]
+├ *${prefix}image* [naruto]
+├ *${prefix}lirik* [judul lagu]
+├ *${prefix}chord* [judul lagu]
+├ *${prefix}igstalk* [barxnl]
+├ *${prefix}wiki* [cinta]
+├ *${prefix}wikien* [love]
+├ *${prefix}tiktokstalk* [username]
+├ *${prefix}url2img* [link]
+├ *${prefix}shorturl*
+├ *${prefix}ssweb* [link]
+├ *${prefix}fototiktok* [username]
+├ *${prefix}map* [kota]
+├ *${prefix}infomotor* [beat mber]
+├ *${prefix}infomobil* [avanza]
+├ *${prefix}indohot* 
+├ *${prefix}kbbi* [kamus]
+├ *${prefix}brainly* [tau sendiri kan]
+├ *${prefix}infoghitub* 
+├ *${prefix}infocuaca* [kota]
+└ *${prefix}infogempa*
+
+_*Group*_
+├ *${prefix}mentionall*
+├ *${prefix}mentionall2*
+├ *${prefix}tagall*
+├ *${prefix}setdesc*
+├ *${prefix}setname*
+├ *${prefix}kick* [tag]
+├ *${prefix}add* [628xxx]
+├ *${prefix}promote* [tag]
+├ *${prefix}demote* [tag]
+├ *${prefix}group* [buka]
+├ *${prefix}group* [tutup]
+├ *${prefix}linkgc*
+├ *${prefix}setpp* [foto kau]
+├ *${prefix}groupinfo*
+├ *${prefix}tagme*
+├ *${prefix}nsfw* [1/0]
+├ *${prefix}openanime* [1/0]
+├ *${prefix}simih* [1/0]
+├ *${prefix}welcome* [1/0]
+├ *${prefix}edotensei*
+├ *${prefix}listadmins*
+└ *${prefix}speed*
+
+_*Primbon*_
+├ *${prefix}cekjodoh* [nama]
+├ *${prefix}ramaljadian* [nama|nama]
+├ *${prefix}primbonjodoh* [text|text]
+├ *${prefix}artinama* [barxnl]
+└ *${prefix}ramalhp* [082xxx]
+
+_*Textpro*_
+├ *${prefix}tahta* [Lindy]
+├ *${prefix}pornohub* [text|text]
+├ *${prefix}snowrite* [text|text]
+├ *${prefix}marvelogo* [text|text]
+├ *${prefix}text3d* [text]
+├ *${prefix}ninjalogo* [text|text]
+├ *${prefix}wolflogo* [text|text]
+├ *${prefix}lionlogo* [text|text]
+├ *${prefix}textscreen* [text
+├ *${prefix}rtext* [text]
+├ *${prefix}thunder* [text]
+├ *${prefix}stiltext* [text|text]
+├ *${prefix}party* [text]
+├ *${prefix}galaxtext* [text]
+├ *${prefix}lovemake* [text]
+├ *${prefix}walpaperhd* [text]
+├ *${prefix}watercolor* [text]
+├ *${prefix}quotemaker* [tx|tx|random]
+├ *${prefix}water* [text]
+├ *${prefix}epep* [text]
+└ *${prefix}glitch* [text]
+
+_*Animals*_
+├ *${prefix}randomcat*
+├ *${prefix}anjing*
+├ *${prefix}elang*
+└ *${prefix}unta*
+
+_*Others*_
+├ *${prefix}nulis*
+├ *${prefix}ocr*
+├ *${prefix}meme*
+├ *${prefix}memindo*
+├ *${prefix}testime*
+├ *${prefix}ttp*
+├ *${prefix}hobby*
+├ *${prefix}watak*
+└ *${prefix}report*
+
+_*Download*_
+├ *${prefix}yt* [link]
+├ *${prefix}ytmp3* [link]
+├ *${prefix}ytmp4* [link]
+├ *${prefix}joox* [judul lagu]
+└ *${prefix}tiktok* [link]
+
+_*Owner*_
+├ *${prefix}bc*
+├ *${prefix}bc2*
+├ *${prefix}leave*
+├ *${prefix}clearall*
+├ *${prefix}clone*
+├ *${prefix}block*
+├ *${prefix}unblock*
+├ *${prefix}hidetag*
+├ *${prefix}hidetag2*
+├ *${prefix}setprefix*
+├ *${prefix}bar*
+├ *${prefix}ban*
+├ *${prefix}getses*
+├ *${prefix}tag*
+╽
+╰╼ Ketik *${prefix}info* untuk melihat list informasi tentang bot
+       Ketik *${prefix}owner* untuk melihat kontak owner
+         Mau donasi? 082198571732(Telkom)
+         Jika tidak ingin donasi bantu Follow Ig aja kak 
+         _instagram.com/barxnl_
+    _*BARXNL_BOT © 2020*_`
+}
+exports.help = help
